11dd2fa865ad765e88505561e7670d5c63b1cf15
Matt Blessed <matthew.blessed@hope.edu>
2017-08-12 13:50:32 -0700

    returning an object for the keypair

--- createAccount.js
+++ createAccount.js
@@ -1,11 +1,7 @@
-
 function CreateKeyPair() {
     // create a completely new and unique pair of keys
     var pair = StellarSdk.Keypair.random();
     // see more about KeyPair objects: https://stellar.github.io/js-stellar-sdk/Keypair.html
 
-    pair.secret();
-    // SAV76USXIJOBMEQXPANUOQM6F5LIOTLPDIDVRJBFFE2MDJXG24TAPUU7
-    pair.publicKey();
-    // GCFXHS4GXL6BVUCXBWXGTITROWLVYXQKQLF4YH5O5JT3YZXCYPAFBJZB
+    return { secret: pair.secret(), publicKey: pair.publicKey() };
 }
