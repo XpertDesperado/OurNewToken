185749ea8178ab6af3ab9a436bc0f5250caa6031
Matt Blessed <matthew.blessed@hope.edu>
2017-08-12 14:20:47 -0700

    update

--- createAccount.js
+++ createAccount.js
@@ -1,28 +1,47 @@
 var request = require('request');
+var Stellar = require('stellar-sdk');
+Stellar.Network.usePublicNetwork();
+
+var server = new Stellar.Server('https://horizon-testnet.stellar.org');
+
+
 
 function createKeyPair() {
     // create a completely new and unique pair of keys
-    var pair = StellarSdk.Keypair.random();
+    var pair = Stellar.Keypair.random();
+
+    // console.log(pair);
     // see more about KeyPair objects: https://stellar.github.io/js-stellar-sdk/Keypair.html
 
     return { secret: pair.secret(), public: pair.publicKey() };
 }
 
-function fundTestAccount(keyPair) {
+function fundTestAccount(keyPair, callback) {
     // The SDK does not have tools for creating test accounts, so you'll have to
     // make your own HTTP request.
     request.get({
         url: 'https://horizon-testnet.stellar.org/friendbot',
-        qs: { addr: pair.publicKey() },
+        qs: { addr: keyPair.public },
         json: true
     }, function(error, response, body) {
-        if (error || response.statusCode !== 200) {
+        callback(body, error);
+    });
+}
+
+function createNewAccount() {
+    var kp = createKeyPair();
+
+    fundTestAccount(kp, function(body, error) {
+        if (error) {
             console.error('ERROR!', error || body);
         }
         else {
             console.log('SUCCESS! You have a new account :)\n', body);
+
+            console.log(kp);
         }
-    });
+    }); 
 }
 
-fundTestAccount(createKeyPair())
+createNewAccount();
+
