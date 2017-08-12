469750d140903972fedae66b12ba67f0ec8d334f
Matt Blessed <matthew.blessed@hope.edu>
2017-08-12 16:09:49 -0700

    update

--- createAToken.js
+++ createAToken.js
@@ -5,6 +5,10 @@ var test1KeyPair = config.testAccount1;
 var test2KeyPair = config.testAccount2;
 var test3KeyPair = config.testAccount3;
 
+// Keys for accounts to issue and receive the new asset
+var issuingKeys = StellarSdk.Keypair.fromSecret(test1KeyPair.secret);
+var receivingKeys = StellarSdk.Keypair.fromSecret(test2KeyPair.secret);
+
 // SDK which uses the test network
 var StellarSdk = require('stellar-sdk');
 StellarSdk.Network.useTestNetwork();
@@ -12,13 +16,11 @@ StellarSdk.Network.useTestNetwork();
 // Server
 var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
-function createToken() {
-    // Keys for accounts to issue and receive the new asset
-    var issuingKeys = StellarSdk.Keypair.fromSecret(test1KeyPair.secret);
-    var receivingKeys = StellarSdk.Keypair.fromSecret(test2KeyPair.secret);
+function createToken(name) {
+    
 
     // Create an object to represent the new asset
-    var skyToken = new StellarSdk.Asset('SkyToken', issuingKeys.publicKey());
+    var skyToken = new StellarSdk.Asset(name, issuingKeys.publicKey());
 
     // First, the receiving account must trust the asset
     server.loadAccount(receivingKeys.publicKey())
@@ -57,3 +59,36 @@ function createToken() {
         console.error('Error!', error);
     });
 }
+
+function trustToken(assetName, issuingAccountPublicKey, receivingAccountKeyPair) {
+    // Create an object to represent the new asset
+    var skyToken = new StellarSdk.Asset(assetName, issuingAccountPublicKey);
+
+    // First, the receiving account must trust the asset
+    server.loadAccount(receivingAccountKeyPair.publicKey())
+    .then(function(receiver) {
+        var transaction = new StellarSdk.TransactionBuilder(receiver)
+
+        // The `changeTrust` operation creates (or alters) a trustline
+        // The `limit` parameter below is optional
+        .addOperation(StellarSdk.Operation.changeTrust({
+            asset: skyToken,
+            limit: '1000'
+        }))
+        .build();
+
+        transaction.sign(receivingAccountKeyPair);
+
+        return server.submitTransaction(transaction);
+    })
+    .catch(function(error) {
+        console.error('Error!', error);
+    });
+}
+
+function sendPayment(assetName, issuingAccountPublicKey, ) {
+    var skyToken = new StellarSdk.Asset(assetName, issuingAccountPublicKey);
+
+}
+
+createToken("SkyToken");
