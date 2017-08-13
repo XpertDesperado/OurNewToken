0997ca29a8046006cc95936c4447852e06329c04
Matt Blessed <matthew.blessed@hope.edu>
2017-08-13 15:03:56 -0700

    update

--- sendPayment.js
+++ sendPayment.js
@@ -9,45 +9,43 @@ var test3KeyPair = config.testAccount3;
 var StellarSdk = require('stellar-sdk');
 StellarSdk.Network.useTestNetwork();
 
-var sourceKeys = StellarSdk.Keypair.fromSecret(test1KeyPair.secret);
+// Keys for accounts to issue and receive the new asset
+var test1Keys = StellarSdk.Keypair.fromSecret(test1KeyPair.secret); // issuer for SkyToken
+var test2Keys = StellarSdk.Keypair.fromSecret(test2KeyPair.secret);
+var test3Keys = StellarSdk.Keypair.fromSecret(test3KeyPair.secret);
 
 // use the test network
 var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
-function sendPayment(amount) {
-    // First, check to make sure that the destination account exists. (skippable, but fee)
-    server.loadAccount(test2KeyPair.public)
-    .catch(StellarSdk.NotFoundError, function (error) {
-        throw new Error('The destination account does not exist!');
-    })
 
-    // If there was no error, load up-to-date information on your account.
-    .then(function() {
-        return server.loadAccount(sourceKeys.publicKey());
-    })
-
+// Batch transaction
+// 5 lumens from Acct1 -> Acct3
+// 2 lumens from Acct3 -> Acct2
+function exercise1() {
+    server.loadAccount(test1Keys.publicKey())
     .then(function(sourceAccount) {
 
         // Start building the transaction.
         var transaction = new StellarSdk.TransactionBuilder(sourceAccount)
         .addOperation(StellarSdk.Operation.payment({
-            destination: test2KeyPair.public,
-            asset: new StellarSdk.Asset("SKyToken", test1KeyPair.public), //StellarSdk.Asset.native(), // Lumens.
-            amount: amount
+            destination: test3KeyPair.public,
+            asset: StellarSdk.Asset.native(), // Lumens.
+            amount: "5"
         }))
-        // .addOperation(StellarSdk.Operation.payment({
-        //     source: test2KeyPair.public,
-        //     destination: test3KeyPair.public,
-        //     asset: StellarSdk.Asset.native(), // Lumens.
-        //     amount: amount
-        // }))
+        .addOperation(StellarSdk.Operation.payment({
+            source: test2KeyPair.public,
+            destination: test3KeyPair.public,
+            asset: StellarSdk.Asset.native(), // Lumens.
+            amount: "2"
+        }))
         // A memo allows you to add your own metadata to a transaction. It's
         // optional and does not affect how Stellar treats the transaction.
         .addMemo(StellarSdk.Memo.text('Test Transaction'))
         .build();
 
         // Sign the transaction to prove you are actually the person sending it.
-        transaction.sign(sourceKeys);
+        transaction.sign(test1Keys);
+        transaction.sign(test2Keys);
 
         // And finally, send it off to Stellar!
         return server.submitTransaction(transaction);
@@ -61,26 +59,8 @@ function sendPayment(amount) {
     });
 }
 
-function buildTransaction(testAccount1KeyPair, testAccount2Public) {
-    // StellarBase.Network.usePublicNetwork(); if this transaction is for the public network
-    // Create an Account object from an address and sequence number.
-    var account = new StellarSdk.Account("GD6WU64OEP5C4LRBH6NK3MHYIA2ADN6K6II6EXPNVUR3ERBXT4AN4ACD","2319149195853854");
+function exercise2() {
 
-    var transaction = new StellarBase.TransactionBuilder(account)
-        // add a payment operation to the transaction
-        .addOperation(StellarBase.Operation.payment({
-            destination: "GASOCNHNNLYFNMDJYQ3XFMI7BYHIOCFW3GJEOWRPEGK2TDPGTG2E5EDW",
-            asset: StellarBase.Asset.native(),
-            amount: "100.50"  // 100.50 XLM
-        }))
-        // add a set options operation to the transaction
-        .addOperation(StellarBase.Operation.setOptions({
-            signer: {
-                ed25519PublicKey: secondAccountAddress,
-                weight: 1
-            }
-        }))
-        .build();
 }
 
-sendPayment("1");
+// exercise1();
